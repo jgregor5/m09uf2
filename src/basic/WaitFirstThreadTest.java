f1224a5b0ca2eb2a7864a5047bb9ca491c098154
julian <julian.gregorio@adhocware.com>
2019-03-06 12:49:57 +0100

    wait first

---
+++ src/basic/WaitFirstThreadTest.java
@@ -0,0 +1,77 @@
+package basic;
+
+import java.util.Random;
+
+/**
+ *
+ * @author julian
+ */
+public class WaitFirstThreadTest {
+ 
+    static final int COUNT = 5;
+    
+    public static void main(String[] args) throws InterruptedException {
+        
+        Result result = new Result();        
+        Thread[] threads = new Thread[COUNT];
+        
+        for (int i=0; i<COUNT; i++) {
+            Worker worker = new Worker(result, i);
+            threads[i] = new Thread(worker, "worker-" + i);
+            threads[i].start();            
+        }
+        
+        System.out.println("MAIN: wait for one thread...");
+        synchronized (result) {
+            result.wait();
+        }        
+        
+        System.out.println("MAIN: result: " + result.get());
+        for (int i=0; i<COUNT; i++) {
+            threads[i].interrupt();
+        }
+    }
+ 
+    static class Result {
+        
+        private int id;
+        
+        public void set(int id) {
+            this.id = id;
+        }
+        
+        public int get() {
+            return id;
+        }
+    }
+    
+    static class Worker implements Runnable {
+        
+        private int id;
+        private Result result;
+        
+        public Worker(Result result, int id) {
+            this.result = result;
+            this.id = id;
+        }
+
+        @Override
+        public void run() {    
+            
+            try {
+                Random r = new Random();
+                Thread.sleep(r.nextInt(10000));
+            } catch (InterruptedException ex) {
+                System.out.println("WORKER: interrupted " + id);
+                return;
+            }
+            
+            System.out.println("WORKER: finished " + id);
+            synchronized (result) {
+                result.notify();
+                result.set(id);
+            }
+            System.out.println("WORKER: notified " + id);
+        }
+    }
+}
