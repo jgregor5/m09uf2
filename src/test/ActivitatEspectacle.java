c07043eb366511ec313dd4d51231a6b85b5cb609
julian <julian.gregorio@adhocware.com>
2019-04-03 13:28:07 +0200

    activitat espectacle

---
+++ src/test/ActivitatEspectacle.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Dissenya una classe thread-safe que permeti reservar entrades.
+ * Un espectacle té un aforament i diverses sessions.
+ * Protegeix el sistema de reserves per evitar race conditions sobre cada 
+ * sessió.
+ * Simula els compradors accedint al sistema de reserves.
+ */
+public class ActivitatEspectacle {
+    
+}
